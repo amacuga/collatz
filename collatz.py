efd6c3f9ab7912416bf87a1608d94d6081736969
Alexandra Macuga <sasenah@gmail.com>
2019-09-22 10:28:28 +0100

    Added README

--- collatz.py
+++ collatz.py
@@ -1,15 +1,17 @@
-# The number we will pwerform the Collatz operations on.
-n = 20
+# Collatz conjecture explained here: https://en.wikipedia.org/wiki/Collatz_conjecture
 
-# Keep looping until mwe reach 1.
+# The number we will perform the Collatz operations on.
+n = int(input("Enter a positive integer: "))
+
+# Keep looping until we reach 1.
 # Note: this assumes the Collatz conjecture is true.
 while n != 1:
     # Print the current value of n.
     print (n)
     # Check if n is even.
     if n % 2 == 0:
-        # If n is even, divide it by two.
-        n = n / 2
+        # If n is even, divide it by two (this will give us integrer instead of floating number).
+        n = n // 2
     else:
         # If n is odd, multiply by three and add 1.
         n = (3 * n) + 1
